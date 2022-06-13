6cff9e8dca1e07603db1f7ea1939b73704f17521
Jack <jack.dowww@gmail.com>
2022-06-13 17:45:28 +1000

    makes memoize strongly typed

--- src/memoize/memoize.ts
+++ src/memoize/memoize.ts
@@ -1,10 +1,10 @@
-export const memoize = (fn: (...args: Array<any>) => any) => {
+export function memoize<F extends (...args: any[]) => any>(fn: F) {
   const cache: Record<any, any> = {};
-  return (...args: Array<any>) => {
+  return (...args: Parameters<F>): ReturnType<F> => {
     const stringifiedArgs = JSON.stringify(args);
     const result = (cache[stringifiedArgs]
         = typeof cache[stringifiedArgs] === 'undefined' ? fn(...args) : cache[stringifiedArgs]);
     return result;
   };
-};
+}
 
