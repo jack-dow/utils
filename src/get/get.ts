275557ce5433b02590e092507ce89149bb60e1e0
Jack Dow <jack.dowww@gmail.com>
2022-06-13 12:06:51 +1000

    fixes eslint not working properly

--- src/get/get.ts
+++ src/get/get.ts
@@ -2,11 +2,11 @@ export function get(
   obj: any,
   path: string | Array<string>,
 ) {
-  let result = obj
-  if (!Array.isArray(path)) path = path.split('.')
+  let result = obj;
+  if (!Array.isArray(path)) path = path.split('.');
 
-  for (const key of path) result = result[key]
+  for (const key of path) result = result[key];
 
-  return result
+  return result;
 }
 
